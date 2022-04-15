4f251bfc17183ae7040df98c34d471d0b92db538
Murilo Ângelo <muriloangelo10@gmail.com>
2022-04-15 18:29:26 -0300

    wip

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: "/",
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        sassOptions: {
+          includePaths: ["./node_modules", "./src/assets"],
+        },
+      },
+    },
+  },
   configureWebpack: {
     resolve: {
       alias: {
@@ -14,4 +25,27 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        // eslint-disable-next-line no-param-reassign
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src",
+        };
+        return options;
+      });
+  },
+  transpileDependencies: ["vue-echarts", "resize-detector"],
 };
